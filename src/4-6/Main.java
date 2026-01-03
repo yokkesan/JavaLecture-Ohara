2e42c65079108cdbb6c4ce35f3488c9c4c700ec8
yokkesan <lc.yteshima@gmail.com>
2026-01-03 13:31:27 +0900

    4-7

---
+++ src/4-6/Main.java
@@ -0,0 +1,19 @@
+// ガベージコレクション
+public class Main {
+    public static void main(String[] args) {
+        boolean jugde = true;
+        if (judge == true) {
+            int[] array = {1, 2, 3};// ifブロック内で配列を作成
+        }
+    }
+}
+
+
+// null
+public class Main {
+    public static void main(String[] args) {
+            int[] array = {1, 2, 3};
+            array = null;
+            array[0] = 10;
+    }
+}
